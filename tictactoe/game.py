0e81e48a7a2d4c4371f3291f26ba0f3eae776b2e
RuoluYe <yeruolu@uw.edu>
2022-11-22 00:38:38 -0800

    get_position, player, start, get_empty spot, bot_move

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,23 +1,22 @@
 ### object Game and player
 from Board import Board
+import random
 
 class Game:
-    def __init__(self, mode: int):
+    def __init__(self):
         self.mode = mode #1 for game bot, 2 for game b/w human
         self.board = Board()
         self.player1 = Human("X")
         self.player2 = None
         self.winner = None
         self.turn = 0
-    
+        self.currentPlayer = random.choice([self.player1, self.player2])
+        
     def set_winner(self, player):
         if self.winner == None:
             self.winner = player
         else:
             print("!!!Multiple winner Error!!!")
-            
-    def get_winner(self):
-        return self.winner
 
     def check_winner(self):
         while self.winner == None:
@@ -42,46 +41,122 @@ class Game:
                     break
             if self.winner != None: break
            
-            player = self.board[1][1]
+            player = self.board.get(1,1)
             if player == None:
                 break
-            diag1 = [self.board[0][0], self.board[2][2]]
+            diag1 = [self.board.get(0,0), self.board.get(2,2)]
             if diag1[0] == player and diag1[1] == player:
                 self.set_winner(player)
                 break
-            diag2 = [self.board[2][0], self.board[0][2]]
+            diag2 = [self.board.get(2,0), self.board.get(0,2)]
             if diag2[0] == player and diag2[1] == player:
                 self.set_winner(player)
                 break
     
-    def start(self):
-        pass
-        
+    def get_position(self):
+        x, y = "", ""
+        while x not in [1,2,3]:
+            x = int(input("which coloumn? left(1), middle(2), or right(3): "))
+        while y not in [1,2,3]:
+            y = int(input("Which row? up(1), middle(2), or down(3): "))
+        return [x, y]
+
+
     
        
-            
-class singleGame(Game):
-    def __init__(self, mode):
-        super().__init__(mode = 1)
-        self.player2 = Bot("0")
-        
-class singleGame(Game):
-    def __init__(self, mode):
-        super().__init__(mode = 1)
-        self.player2 = Human("0")
-
-
-
-class Player:
+class Player():
     def __init__(self, id) :
         self.id = id
+    def other_player(self):
+        """Given the character for a player, returns the other player."""
+        assert self.id == "X" or self.id == "0", "Player must be 'X' or '0'"
+        if self.id == "X": 
+            self.id = "0" 
+        else: 
+            self.id = "X"
         
 class Human(Player):
-    def __init__(self):
-        super().__init__()
+    pass
         
 class Bot(Player):
-    def __init__(self):
-        super().__init__()
+    pass
 
+class doubleGame(Game):
+    def __init__(self, mode: int):
+        super().__init__(mode)
+        self.player2 = Human("0")
+    
+    def start(self):
+        while self.winner is None:
+            self.turn += 1
+            print(self.board)
+            
+            print(self.currentPlayer + ", your turn")
+            
+            positions = self.get_position()
+            while self.board.is_filled(positions[0] - 1,positions[1] - 1):
+                positions = self.get_position()
+            x, y = positions[0], positions[1]
+            print(f"{self.currentPlayer} chose {positions[0]}, {positions[1]}")
+            
+            self.board.set(x - 1, y - 1, self.currentPlayer.id)
+            print(self.board)
+            
+            self.currentPlayer.other_player()
+            
+            if self.turn > 3:
+                self.check_winner()
+            if self.turn == 9 and self.winner is None:
+                break
         
+        if self.winner:
+            print("We have a winner, " + self.winner + "!")
+        else:
+            print("There is a tie")
+        
+class singleGame(Game):
+    def __init__(self, mode: int):
+        super().__init__(mode)
+        self.player2 = Bot("0")
+        self.human_turn = random.choice([True, False])
+    
+    def get_empty_spot(self):
+        spots = []
+        for y in range(3):
+            for x in range(3):
+                if self.board.get(x,y) is None:
+                    spots.append([x,y])
+        return spots
+
+    def bot_move(self):
+        spots = self.get_empty_spot()
+        return spots[random.choice(spots)]
+    
+    def start(self):
+        while self.winner is None:
+            if not self.human_turn:
+                botMove = self.bot_move()
+                self.board.set(botMove[0], botMove[1], "0") # Bot.id
+                print(f"Bot move to {botMove[0]+1},{botMove[1]+1}") 
+                print(self.board)
+            else:
+                print("Your turn now!")
+                
+                positions = self.get_position()
+                while self.board.is_filled(positions[0] - 1,positions[1] - 1):
+                    positions = self.get_position()
+                x, y = positions[0], positions[1]
+                print(f"You chose {x}, {y}")
+            
+                self.board.set(x - 1, y - 1, self.currentPlayer.id)
+                print(self.board) 
+        
+            if self.turn > 3:
+                    self.check_winner()
+            if self.turn == 9 and self.winner is None:
+                    break
+            
+            if self.winner:
+                print("We have a winner, " + self.winner + "!")
+            else:
+                print("There is a tie")
