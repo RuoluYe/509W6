a656198c5cf464ad96907c92af1f9d373bfdeda5
RuoluYe <yeruolu@uw.edu>
2022-11-29 20:12:14 -0800

    +efficiency, relocate functions to classes.

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,23 +1,36 @@
 ### object Game and player
-from Board import Board
+from board import Board
 import random
 import pandas as pd
 
 class Game:
     def __init__(self):
         self.board = Board()
-        self.player1 = Human("X")
-        self.player2 = Human("0")
         self.winner = None
         self.turn = 0
         self.savedGames: pd.DataFrame = self.read_games()
         self.savedMoves: pd.DataFrame = self.read_moves()
+        self.player1 = Bot()
+        self.player2 = Bot()
+        self.currentPlayer = self.player1
 
+    def other_player(self):
+        """Given the character for a player, returns the other player."""
+        if self.currentPlayer == self.player1:
+            self.currentPlayer = self.player2
+        else: 
+            self.currentPlayer = self.player1
 
-    def finishing(self):
+    def finish(self):
         if self.winner:
             print(self.board)
-            print("We have a winner, " + str(self.winner) + "!")
+            id = str(self.winner) 
+            if id == "X": 
+                self.winner = self.player1
+            else:
+                self.winner = self.player2
+             
+            print("We have a winner, " + self.winner.name + "!")
         else:
             print("There is a tie")
         self.add_game()
@@ -26,7 +39,7 @@ class Game:
 
         
         
-    def read_games():
+    def read_games(self):
         try:
             return pd.read_csv("game.csv")
         except FileNotFoundError:
@@ -37,7 +50,7 @@ class Game:
                 "Winner",
                 "Turns taken",
         ])
-    def read_moves():
+    def read_moves(self):
         try:
             return pd.read_csv("moves.csv")
         except FileNotFoundError:
@@ -47,13 +60,15 @@ class Game:
                 "Player",
                 "Position",
             ])
-    def add_game(sel):
+    
+    def add_game(self):
         length = len(self.savedGames)
         self.savedGames.loc[length] = {
         "Game ID": length+1,
         "Player 1": self.player1.name, 
         "Player 2": self.player2.name, 
-        "Winner": self.winner
+        "Winner": self.winner.name,
+        "Turns taken": self.turn,
     }
         
     def add_move(self, player, position):
@@ -70,114 +85,106 @@ class Game:
         if self.winner == None:
             self.winner = player
         else:
-            print("!!!Multiple winner Error!!!")
+            print("Multiple winner Error!")
 
+        
     def check_winner(self):
-        for i in range(3):
-            thisRow = self.board.get_row(i)
+        
+        def check_winner_helper(positions):
+            player = positions[0]
+            if player == None:
+                return False
+            if positions[0] == positions[1] and positions[0] == positions[2]:
+                self.set_winner(player)
+                return True
+            return False
             
-            player = thisRow[0]
-            if player == None:
-                continue
-            if thisRow[0] == thisRow[1] and thisRow[0] == thisRow[2]:
-                self.set_winner(player)
-                return True
-                
         for i in range(3):
-            thisCol = self.board.get_col(i)
-            player = thisCol[0]
-            if player == None:
-                continue
-            if thisCol[0] == thisCol[1] and thisCol[0] == thisCol[2]:
-                self.set_winner(player)
-                return True
+            if self.winner == None:
+                thisRow = self.board.get_row(i)
+                thisCol = self.board.get_col(i)
+                check_winner_helper(thisRow)
+                check_winner_helper(thisCol)
         
         player = self.board.get(1,1)
-        if player == None:
+        if self.winner != None:
+            return True
+        elif player == None: 
             return False
-        diag1 = [self.board.get(0,0), self.board.get(2,2)]
-        if diag1[0] == player and diag1[1] == player:
-            self.set_winner(player)
-            return True
-        diag2 = [self.board.get(2,0), self.board.get(0,2)]
-        if diag2[0] == player and diag2[1] == player:
-            self.set_winner(player)
-            return True
-    
-
-
-    def update_game(self, winner, filename = "game.csv"):
-        games = pd.read_csv(filename)
-        games.loc[len(games)]= {
-        "Game ID": len(games)+1,
-        "Player 1": self.player1, 
-        "Player 2": self.player2, 
-        "Winner": winner
-    }
-        
-    
-       
-class Player():
-    def __init__(self, id) :
-        self.id = id
-    
-    def __str__(self):
-        return str(self.id)
-
-    def other_player(self):
-        """Given the character for a player, returns the other player."""
-        assert self.id == "X" or self.id == "0", "Player must be 'X' or '0'"
-        if self.id == "X": 
-            self.id = "0" 
-        else: 
-            self.id = "X"
-        
-class Human(Player):
-    name = input("Please input your name:")
-    def get_move(self):
-        x = int(input("which coloumn? left(1), middle(2), or right(3): "))
-        y = int(input("Which row? up(1), middle(2), or down(3): "))
-        nums = [1,2,3]
-        if (x not in nums) or (y not in nums):
-            print("Please only enter 1,2, or 3 for your x & y input.")
-            return self.get_position()
-        if (self.board.is_filled(x-1,y-1)):
-            print("Spot taken, please re-select!")
-            return self.get_position()
-        return [x, y]
-        
-class Bot(Player):
-    name = "Bot"
-    def get_move(self, b: Board()):
-        spots = b.get_empty_spot()
-        return random.choice(spots)
-
-
-class doubleGame(Game):
-    def __init__(self):
-        super().__init__()
-        self.player2 = Human("0")
-        self.currentPlayer = random.choice([self.player1, self.player2])
+        else:
+            diag1 = [self.board.get(0,0), self.board.get(2,2)]
+            if diag1[0] == player and diag1[1] == player:
+                self.set_winner(player)
+                return True
+            diag2 = [self.board.get(2,0), self.board.get(0,2)]
+            if diag2[0] == player and diag2[1] == player:
+                self.set_winner(player)
+                return True 
     
     def start(self):
         while self.winner is None:
             self.turn += 1
             print(self.board)
+
+            if self.currentPlayer.type == "Human":
+                print("Your turn now!" +
+                      str(self.currentPlayer.name))                
             
-            print(str(self.currentPlayer) + ", your turn")
-            
-            positions = self.get_position()
-            x, y = positions[0], positions[1]
-            print(f"{self.currentPlayer} chose {positions[0]}, {positions[1]}")
-            
-            self.board.set(x - 1, y - 1, self.currentPlayer.id)
-            
-            self.currentPlayer.other_player()
-            
+            move = self.currentPlayer.get_move(self.board)
+            x = move[0]
+            y = move[1]
+            self.board.set(x, y, self.currentPlayer.id)
+            self.add_move(self.currentPlayer.name, move)
+        
             if self.turn > 3:
                 self.check_winner()
             if self.turn == 9 and self.winner is None:
                 break
+            self.other_player()
+        
+        self.finish()
+        
+class Player():
+    def __init__(self, id = None) :
+        self.id = id
+    
+    def __str__(self):
+        return str(self.id)
+
+
+        
+class Human(Player):
+    name = input("Please input your name:")
+    type = "Human"
+    def get_move(self, board):
+        x = int(input("which coloumn? left(1), middle(2), or right(3): "))
+        y = int(input("Which row? up(1), middle(2), or down(3): "))
+        nums = [1,2,3]
+        if (x not in nums) or (y not in nums):
+            print("Please only enter 1,2, or 3 for your x & y input.")
+            return self.get_move()
+        if (board.is_filled(x-1,y-1)):
+            print("Spot taken, please re-select!")
+            return self.get_move()
+        print(f"{self.name} chose {x}, {y}")
+        return [x-1, y-1]
+        
+class Bot(Player):
+    name = "Bot"
+    type = "Bot"
+    def get_move(self, board):
+        spots = board.get_empty_spot()
+        spot = random.choice(spots)
+        print(f"Bot move to {spot[0]+1},{spot[1]+1}") 
+        return spot
+
+
+class doubleGame(Game):
+    def __init__(self):
+        super().__init__()
+        self.player1 = Human("X")
+        self.player2 = Human("0")
+        self.currentPlayer = random.choice([self.player1, self.player2])
         
  
         
@@ -185,35 +192,9 @@ class doubleGame(Game):
 class singleGame(Game):
     def __init__(self):
         super().__init__()
+        self.player1 = Human("X")
         self.player2 = Bot("0")
-        self.human_turn = random.choice([True, False])
-    
-    
-    
-    def start(self):
-        while self.winner is None:
-            self.turn += 1
-            if not self.human_turn:
-                move = self.player2.get_move(self.board)
-                print(f"Bot move to {move[0]+1},{move[1]+1}") 
-                
-                print(self.board)
-            else:
-                print("Your turn now! (((" + str(self.winner))
-                move = self.player1.get_move()
-                
-                print(f"You chose [{x}, {y}]")
-            
-            self.board.set(x - 1, y - 1, self.player1.id)
-            #add move
-            print(self.board) 
-        
-            if self.turn > 3:
-                self.check_winner()
-            if self.turn == 9 and self.winner is None:
-                break
-            self.human_turn = not self.human_turn
-        #add game
+        self.currentPlayer = random.choice([self.player1,  self.player2])
         
             
 
