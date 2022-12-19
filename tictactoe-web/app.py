5c247d574a6f8ace8850f9fc0c03c99dee1f7bac
Lu Ye <yeruolu@uw.edu>
2022-12-18 21:28:47 -0800

    session

--- tictactoe-web/app.py
+++ tictactoe-web/app.py
@@ -2,62 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from tempfile import mkdtemp
 import random
 
-class Board:
-    def __init__(self):
-        self._rows = [
-            [None, None, None],
-            [None, None, None],
-            [None, None, None]
-        ]
-
-    def __str__(self):
-        s = '-------\n'
-        for row in self._rows:
-            for cell in row:
-                s = s + '|'
-                if cell == None:
-                    s = s + ' '
-                else:
-                    s = s + cell
-            s = s + '|\n-------\n'
-        return s
-    
-    def get(self, x: int, y: int):
-        return self._rows[y][x]
-
-    def is_filled(self, x, y):
-        if self.get(x,y) is None:
-            return False
-        return True
-
-    def set(self, x: int, y: int, value):
-        self._rows[y][x] = value
-        
-    def get_row(self, row: int):
-        return self._rows[row]
-    
-    def get_col(self, col: int): 
-        # col = 1,2,or 3 for first,second or third column
-        b = self._rows
-        return [b[0][col], b[1][col], b[2][col]]
-    
-    def get_empty_spot(self):
-        spots = []
-        for y in range(3):
-            for x in range(3):
-                if self.get(x,y) is None:
-                    spots.append([x,y])
-        return spots 
-
 app = Flask(__name__) #create a flask from this file
 app.secret_key = "ttt"
 
 @app.route('/', methods = ['GET', 'POST'])
 def get_game_mode():
     session.pop("name", None)
+    session.pop("board", None)
+    person = random.choice(["X","0"])
     if request.method == 'POST':
         name = request.form["single user name"]
         session["name"]= name
+        if "board" not in session:
+            session["board"] = [
+                [None, None, None],
+                [None, None, None],
+                [None, None, None]
+            ]
+            session["turn"] = person
+            session["winner"] = False
+            session["draw"] = False
         return redirect(url_for("game"))
         # return render_template('game.html', name = name)
     else: 
@@ -70,13 +34,79 @@ def get_game_mode():
 
 @app.route('/game', methods = ["POST", "GET"])
 def game():
+    
+    
+    winner = get_winner(session["board"])
+        
     if "name" in session:
         name = session["name"]
-        session
         return render_template('game.html', name = name)
+        # return render_template('game.html', name = name, game = session["board"], turn=session["turn"], winner = winner)
     else:
         return redirect(url_for("get_game_mode"))
 
+def get_winner(board):
+    win = None
+    n = len(board)
+    player = None
+    # check rows
+    for i in range(n):
+        win = True
+        player = board[i][0] # get the player of  the first one of this row. 
+        for j in range(n):
+            if board[i][j] != player:
+                win = False
+                break # this row fail, check next row
+        if win:
+            return player
+    #check columns
+    for j in range(n):
+        win = True
+        player = board[0][j] # get the player as the first one of each column.
+        for i in range(n):
+            if board[i][j] != player:
+                win = False
+                break # this column fail, check next
+        if win:
+            return player
+    # check diag
+    player = board[0][0]
+    #top left bottom right
+    win = True
+    for i in range(1,n):
+        if board[i][i] != player:
+            win = False
+            break
+    if win: 
+        return player
+    #top right bottom left
+    player = board[0][2]
+    win = True
+    for i in range(1,n):
+        if board[i][n-1-i] != player:
+            win = False
+            break
+    if win:
+        return player
+    return None
 
+@app.route('/debug')
+def debug():
+    turn = 1
+    if "turn" in session:
+        turn = session["turn"]
+    return render_template('debug.html', game = session["board"], turn = turn)
+
+@app.route("/game/<int:row>/<int:col>")
+def play(row, col):
+    session["board"][row][col] = session["turn"]
+    if session["turn"] == "X":
+        session["turn"] = "0"
+    else:
+        session["turn"] = "X"
+    return redirect(url_for('debug'))
+    
 if __name__ == "__main__":
     app.run(debug = True)
+    
+    
